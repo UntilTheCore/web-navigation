a4edc0ffd20ce466c345063cd5152151dd024c77
UntilTheCore <814898862@qq.com>
2020-07-01 18:39:14 +0800

    修复移动端上的bug，完成PC端适配

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -119,22 +119,201 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   return newRequire;
 })({"index.js":[function(require,module,exports) {
 $('.searchForm').on('submit', function (e) {
+  return startSearch();
+});
+$('.icon-search').on('click', function (e) {
+  startSearch();
+});
+
+function startSearch() {
   var val = $('.search input').val().trim();
+
+  if ('' === val) {
+    return false;
+  }
+
   window.open("https://baidu.com/s?wd=".concat(val));
   return false;
-});
+} // 获取站点 logo 地址
+
+
 $('.addBtn').click(function (e) {
-  var str = prompt('请输入要添加的地址!');
-  $('<li><div class="div-url"><svg class="icon-close">' + '<use xlink:href="#iconclose1"></use>' + '</svg></div><span></span></li>').insertBefore('.addBtn');
-});
+  var src = prompt('请输入要添加的地址!');
+  var spanStr;
+  var urlObject = getLocalStorage();
+  urlObject = urlObject === null ? {} : urlObject;
+
+  if (src) {
+    if (checkChinese(src)) return alert('请输入正确的域名！');
+    src = src.trim().toLowerCase();
+
+    if (!src.startsWith('https://')) {
+      src = 'https://' + src;
+    }
+
+    spanStr = src.slice(8).split('.');
+    spanStr = spanStr.length > 2 ? spanStr[1] : spanStr[0]; // 是否有相同的链接存在
+
+    for (key in urlObject) {
+      if (compare(urlObject[key], src)) {
+        alert('链接已存在');
+        return;
+      }
+    }
+
+    urlObject[spanStr] = src;
+    setLocalStorage(urlObject);
+    createElement(src, spanStr);
+  }
+}); // 中文检测
+
+function checkChinese(val) {
+  var reg = new RegExp("[\\u4E00-\\u9FFF]+", "g");
+
+  if (reg.test(val)) {
+    return true;
+  } else {
+    return false;
+  }
+} // 1、隐藏 li
+// 2、删除 localStorage 中对应的键
+
+
 $('.list > ul').on('click', '.icon-close', function (e) {
-  var parent = $(e.target).parents('li').hide('slow');
-  console.log(parent);
+  var isTouchDevice = ('ontouchstart' in document.documentElement);
+
+  if (isTouchDevice) {
+    $(e.target).parents('li').hide('slow');
+  } else {
+    $(e.target).parents('li').fadeOut('slow');
+  } // 后续更新span的值以用户输入的为主，所以取 background-image 的值来判断
+
+
+  var value = $(e.target).parents('div').children('.bgc-div').css('backgroundImage'); // 直接获取的值为 url("xxxx") 的字符串,要从 " 开始分割
+
+  value = value.split("\"")[1].split("\"")[0];
+  value = value.slice(0, -12);
+  var key = findKey(value, compare);
+  var urls = getLocalStorage();
+  key && delete urls[key] && setLocalStorage(urls);
   e.stopPropagation();
 });
-$('.list > ul').on('click', '.div-url', function (e) {
-  console.log(123);
+
+var compare = function compare(a, b) {
+  return a === b;
+};
+
+function findKey(value, compare) {
+  var urls = getLocalStorage();
+
+  for (key in urls) {
+    if (compare(urls[key], value)) {
+      return key;
+    }
+  }
+}
+
+function setLocalStorage(value) {
+  localStorage.setItem('urls', JSON.stringify(value));
+}
+
+function getLocalStorage() {
+  return JSON.parse(localStorage.getItem('urls'));
+}
+
+var isCloseBtnShow = false;
+var startTime;
+var endTime;
+var timeStep = 0; // 移动端长按显示删除按钮
+
+$('.list > ul').on({
+  'touchstart': function touchstart(e) {
+    startTime = new Date();
+  },
+  'touchend': function touchend() {
+    endTime = new Date();
+    var touchTime = endTime - startTime;
+
+    if (touchTime > 400) {
+      showCloseBtn();
+    }
+  }
+}); // PC 端长按显示删除按钮
+
+$('.list > ul').on('mousedown', 'li', function (e) {
+  if (0 === e.button) {
+    startTime = new Date();
+  }
 });
+$('.list > ul').on('mouseup', 'li', function (e) {
+  if (0 === e.button) {
+    endTime = new Date();
+    timeStep = endTime - startTime;
+
+    if (timeStep > 500) {
+      showCloseBtn();
+    }
+  }
+}); // 点击任意地方隐藏删除按钮
+
+$('.container').on('mousedown', function (e) {
+  if (2 === e.button) {
+    if (isCloseBtnShow) {
+      $('.list .icon-close').css({
+        'display': 'none'
+      });
+      e.stopPropagation();
+      isCloseBtnShow = false;
+    }
+  }
+}); // 响应点击跳转
+
+$('.list > ul').on('click', '.li-url', function (e) {
+  if (timeStep < 100) {
+    var url = $(e.target).parents('.li-url').data('url');
+    window.open(url);
+  }
+}); // 阻止长按出现菜单
+
+$('ul').on('contextmenu', function (e) {
+  e.preventDefault();
+});
+
+function showCloseBtn() {
+  $('.list .icon-close').css({
+    'display': 'block'
+  });
+  isCloseBtnShow = true;
+}
+
+$(document).ready(function () {
+  var urls = getLocalStorage();
+
+  for (key in urls) {
+    createElement(urls[key], key);
+  }
+}); // 创建节点
+// 为避免使用 img 在安卓浏览器长按出现图片下载菜单，使用背景图
+
+function createElement(imgSrc, spanStr) {
+  var image = new Image();
+  image.src = imgSrc + "/favicon.ico";
+  $("<li class=\"li-url\" data-url=".concat(imgSrc, ">\n              <div>\n                 <h1>").concat(spanStr.slice(0, 1).toUpperCase(), "</h1>\n                 <div class=\"bgc-div\" style=\"background-image: url('").concat(imgSrc + "/favicon.ico", "')\" ></div>\n                 <svg class=\"icon-close\"> \n                    <title>\u5220\u9664\u6B64\u94FE\u63A5</title>\n                    <use xlink:href=\"#iconclose1\"></use> \n                </svg>\n              </div>\n              <span>").concat(spanStr, "</span>\n           </li>")).insertBefore('.addBtn');
+  $(image).on('load', function (e) {
+    $('ul > li').each(function (index, element) {
+      var el = $(element);
+
+      if (el.data('url') === imgSrc) {
+        el.find('h1').css({
+          'display': 'none'
+        });
+        el.find('.bgc-div').css({
+          'display': 'block'
+        });
+      }
+    });
+  });
+}
 },{}],"../../../../../../AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -163,7 +342,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "5691" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "2239" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/index.js
+++ src/index.js
@@ -1,25 +1,196 @@
 $('.searchForm').on('submit', e => {
+    return startSearch()
+})
+$('.icon-search').on('click',e => {
+   startSearch()
+})
+
+function startSearch() {
     let val = $('.search input').val().trim()
+    if('' === val){
+        return false
+    }
     window.open(`https://baidu.com/s?wd=${val}`)
     return false
-})
+}
 
+// 获取站点 logo 地址
 $('.addBtn').click(e => {
-    let str = prompt('请输入要添加的地址!')
-    $('<li><div class="div-url"><svg class="icon-close">' +
-        '<use xlink:href="#iconclose1"></use>' +
-        '</svg></div><span></span></li>').insertBefore('.addBtn')
+    let src = prompt('请输入要添加的地址!')
+    let spanStr
+    let urlObject = getLocalStorage()
+    urlObject = urlObject === null ? {} : urlObject
+    if(src){
+        if(checkChinese(src)) return alert('请输入正确的域名！')
+        src = src.trim().toLowerCase()
+        if(!src.startsWith('https://')){
+            src = 'https://' + src
+        }
+        spanStr = src.slice(8).split('.')
+        spanStr = spanStr.length > 2 ? spanStr[1] : spanStr[0]
+        // 是否有相同的链接存在
+        for(key in urlObject){
+           if(compare(urlObject[key],src)){
+               alert('链接已存在')
+               return
+           }
+        }
+        urlObject[spanStr] = src
+        setLocalStorage(urlObject)
+        createElement(src,spanStr)
+    }
 })
 
+// 中文检测
+function checkChinese(val){
+    var reg = new RegExp("[\\u4E00-\\u9FFF]+","g");
+    if(reg.test(val)){
+        return true
+    }else {
+        return false
+    }
+}
+
+// 1、隐藏 li
+// 2、删除 localStorage 中对应的键
 $('.list > ul').on('click', '.icon-close', e => {
-    let parent = $(e.target).parents('li').hide('slow')
-    console.log(parent)
+    let isTouchDevice = 'ontouchstart' in document.documentElement
+    if(isTouchDevice){
+        $(e.target).parents('li').hide('slow')
+    }else {
+        $(e.target).parents('li').fadeOut('slow')
+    }
+
+    // 后续更新span的值以用户输入的为主，所以取 background-image 的值来判断
+    let value = $(e.target).parents('div').children('.bgc-div').css('backgroundImage')
+    // 直接获取的值为 url("xxxx") 的字符串,要从 " 开始分割
+    value = value.split("\"")[1].split("\"")[0];
+    value = value.slice(0,-12)
+    let key = findKey(value,compare)
+    let urls = getLocalStorage()
+    key && delete urls[key] && setLocalStorage(urls)
     e.stopPropagation()
 })
 
-$('.list > ul').on('click','.div-url',e => {
-    console.log(123)
+let compare = (a,b) => a === b
+function findKey(value,compare) {
+    let urls = getLocalStorage()
+    for(key in urls){
+        if(compare(urls[key],value)){
+            return key
+        }
+    }
+}
+
+function setLocalStorage(value){
+   localStorage.setItem(('urls'),JSON.stringify(value))
+}
+
+function getLocalStorage(){
+    return JSON.parse(localStorage.getItem('urls'))
+}
+
+let isCloseBtnShow = false
+let startTime
+let endTime
+let timeStep = 0
+
+// 移动端长按显示删除按钮
+$('.list > ul').on({
+    'touchstart':function(e) {
+        startTime = new Date()
+    },
+    'touchend':function() {
+        endTime = new Date()
+        let touchTime = endTime - startTime
+        if(touchTime > 400){
+            showCloseBtn()
+        }
+    }
+})
+
+// PC 端长按显示删除按钮
+$('.list > ul').on('mousedown','li',
+    e => {
+        if(0 === e.button){
+            startTime = new Date()
+        }
+    }
+)
+
+$('.list > ul').on('mouseup','li',e => {
+    if(0 === e.button) {
+        endTime = new Date()
+        timeStep = endTime - startTime
+        if(timeStep > 500) {
+            showCloseBtn()
+        }
+    }
+})
+
+// 点击任意地方隐藏删除按钮
+$('.container').on('mousedown',e => {
+    if(2 === e.button){
+        if(isCloseBtnShow) {
+            $('.list .icon-close').css({'display' : 'none'})
+            e.stopPropagation()
+            isCloseBtnShow = false
+        }
+    }
+})
+
+// 响应点击跳转
+$('.list > ul').on('click','.li-url',e => {
+    if(timeStep < 100) {
+        let url = $(e.target).parents('.li-url').data('url')
+        window.open(url)
+    }
+})
+
+// 阻止长按出现菜单
+$('ul').on('contextmenu',e => {
+    e.preventDefault()
 })
 
 
+function showCloseBtn(){
+    $('.list .icon-close').css({'display': 'block'})
+    isCloseBtnShow = true
+}
 
+$(document).ready(function() {
+    let urls = getLocalStorage()
+    for(key in urls){
+        createElement(urls[key],key)
+    }
+})
+
+
+// 创建节点
+// 为避免使用 img 在安卓浏览器长按出现图片下载菜单，使用背景图
+function createElement(imgSrc,spanStr) {
+    let image = new Image()
+    image.src = imgSrc + "/favicon.ico"
+
+    $(`<li class="li-url" data-url=${imgSrc}>
+              <div>
+                 <h1>${spanStr.slice(0,1).toUpperCase()}</h1>
+                 <div class="bgc-div" style="background-image: url('${imgSrc + "/favicon.ico"}')" ></div>
+                 <svg class="icon-close"> 
+                    <title>删除此链接</title>
+                    <use xlink:href="#iconclose1"></use> 
+                </svg>
+              </div>
+              <span>${spanStr}</span>
+           </li>`).insertBefore('.addBtn')
+    $(image).on('load',e => {
+        $('ul > li').each((index,element) => {
+            let el = $(element)
+            if(el.data('url') === imgSrc){
+                el.find('h1').css({'display' : 'none'})
+                el.find('.bgc-div').css({'display' : 'block'})
+            }
+        })
+    })
+
+}
